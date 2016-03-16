46a9f81403c6b9ccb20f52dfdbdf1bb0fb63a934
Stefan Jacholke <stefanjacholke@gmail.com>
2016-03-16 15:08:06 +0200

    nine

--- algos/ninepuzzle.hs
+++ algos/ninepuzzle.hs
@@ -1,50 +1,55 @@
-import Data.Array
+import qualified Data.Vector as V
 import Control.Monad
-import Data.Maybe (isJust, Maybe, fromJust)
 
+--  (Board, positionOfZero)
+type BoardState = (Board, Pos)
+type Board = V.Vector Int 
+type Pos = (Int, Int)
 instance (Num a, Num b) => Num (a,b) where
     (+) (q,w) (a,s) = (q+a,w+s)
-
-genState seque = array ((0,0),(3,3)) $ let i = liftM2 (,) [0..3] [0..3]
-                                in zip i seque
-
-testArr = genState [9,2,8,11,0,5,13,7,15,1,4,10,3,14,6,12]
-endState = genState [9,2,8,11,5,1,13,7,15,0,4,10,3,14,6,12]
-
-genNeighbors state = let
-        (zeroPos, _) = head $ filter (\(i,e) -> e==0) $ assocs state
-    in map (\s -> swap state s zeroPos) $ genSwaps zeroPos
-    where
-        genSwaps a@(i,j) = filter (\(q,w) -> let d = (abs (q-i) + (abs (w-j)))
-            in d == 1 && q >= 0 && q < 4 && w >= 0 && w < 4) $
-                map (\b -> a+b) $ liftM2 (,) [-1,1,0] [1,-1,0]
-        swap state a b = let    av = state ! a
-                                bv = state ! b
-                        in state // [(a,bv),(b,av)]
-
--- && q >= 0 && q < 4 && w >= 0 && w < 4
-search state end _  moves
-    | state == end = Just moves
-
-search state end visited moves
-    | neighbors == [] = Nothing
-    | otherwise = head $ (filter (\a -> isJust a) $ map
-            (\a -> search a end (state:visited) (state:moves)) neighbors) ++ [Nothing]
-    where neighbors = filter (not . flip elem visited) $ genNeighbors state
+startState = (V.fromList [1,8,2,0,4,3,7,6,5], (1,0)) :: BoardState
+goalState = (V.fromList [1,2,3,4,5,6,7,8,0], (2,2)) :: BoardState
 
 
+genSwaps a@(i,j) = filter (\(q,w) -> let d = (abs (q-i) + (abs (w-j)))
+            in d == 1 && q >= 0 && q < 3 && w >= 0 && w < 3) $
+                fmap (\b -> a+b) $ liftM2 (,) [-1,1,0] [1,-1,0]
+swaps' = V.fromList $ map genSwaps [(i,j) | i <- [0..2], j<- [0..2]]
+swap (i,j) = swaps' V.! (i*3 + j)
 
-visualize state =   let e = map (\i -> i ++ "\t") $ map show $ elems state
-                        xs = splitter e 4
-                        s = map unwords xs
-                    in unlines s
+(!) :: Board -> Pos -> Int
+board ! p = board V.! (o p)
 
-visualize' state = putStr $ visualize state
 
 splitter [] _ = []
 splitter lis i = let (h,t) = splitAt i lis
     in h:splitter t i
+visualize :: Board -> String
+visualize board =   let e = map (\i -> i ++ "\t") $ map show $ V.toList board
+                        xs = splitter e 3
+                        s = map unwords xs
+                    in unlines s
 
-main = do
-        let s = fromJust $ search testArr endState [] []
-        putStrLn $ unlines $ map visualize s
+visualize' board = let (b,_) = board in putStr $ visualize b
+
+-- o :: (Int, Int) -> Int
+o (i,j) = (i*3 + j)
+-- d :: (Int) -> (Int, Int)
+d k = (k `div` 3, k `mod` 3) 
+
+branch :: BoardState -> [BoardState]
+branch bs = zip (map (\n -> swapBoard board zpos n) neighbors) neighbors
+    where neighbors = swap zpos 
+          (board,zpos) = bs
+          swapBoard :: Board -> Pos -> Pos -> Board
+          swapBoard state a b = let av = state ! a
+                                    bv = state ! b
+                        in state V.// [(o a,bv),(o b,av)]
+
+
+
+playLoop boardState = do
+  visualize' boardState
+  putStr "\nEnter a move in the form of
+  [i,j] <- getLine >>= return $ words
+  `
